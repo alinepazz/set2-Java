aa8a7b36ef45025d889a0c7bb615bd16e78110c4
Aline Paz <aline.soares.2@hotmail.com>
2022-01-28 19:55:45 -0300

    Criando o programa principal

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,50 @@
 package application;
 
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Set;
+import entities.Usuario;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Set<Usuario> set = new HashSet<>();
+		
+		System.out.print("How many students for course A? ");
+		int n = sc.nextInt();
+		
+		for(int i = 1; i<= n;  i++) {
+			int codigo1 = sc.nextInt();
+			
+			set.add(new Usuario(codigo1));
+		}
+		
+		System.out.print("How many students for course B? ");
+		int n2 = sc.nextInt();
+		
+		for(int i = 1; i<= n2;  i++) {
+			int codigo2 = sc.nextInt();
+			
+			set.add(new Usuario(codigo2));
+		}
+		
+		System.out.print("How many students for course C? ");
+		int n3 = sc.nextInt();
+		
+		for(int i = 1; i<= n3;  i++) {
+			int codigo3 = sc.nextInt();
+			
+			set.add(new Usuario(codigo3));
+		}
+		
+		System.out.println("Total students: " + set.size());
+		
+		sc.close();
 
 	}
 
